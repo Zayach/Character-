6ee05d90873ad131d7aade07bc499cb565ade87d
Zayach <youngzachary777@gmail.com>
2019-04-27 20:36:25 -0500

    Add files via upload

---
+++ Character++/Character++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main(){
+    vector<string> race["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"];
+    vector<string> charClass["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
+    vector<string> allignment["Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "Neutral", "Chaotic Neutral", "Lawful Evil", "Neutral Evil", "Chaotic Evil"];
+    vector<string> background["Acolyte", "Charlatan", "Criminal", "Entertainer", "Folk Hero", "Guild Artisan", "Hermit", "Noble", "Outlander", "Sailor", "Soldier", "Urchin"];
+}
