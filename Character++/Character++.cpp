640a2ff7a91289a40aedb9e670cb4d0919b5bc2b
Zayach <youngzachary777@gmail.com>
2019-04-29 17:24:45 -0500

    Update Character++.cpp

--- Character++/Character++.cpp
+++ Character++/Character++.cpp
@@ -3,6 +3,14 @@
 #include <time.h>
 using namespace std;
 
+int diceRoll(int numDice, int sidesDice){
+    int diceTotal = 0;
+    for(int i = 0; i < numDice; i++){
+        diceTotal = (rand()%sidesDice+1) + diceTotal;
+    }
+    return diceTotal;
+}
+
 int main(){
     vector<string> race["Dwarf", "Elf", "Halfling", "Human", "Dragonborn", "Gnome", "Half-Elf", "Half-Orc", "Tiefling"];
     vector<string> charClass["Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"];
